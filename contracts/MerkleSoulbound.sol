2984271f2a80dbc2ddebccfb7b92068057c36f9e
qedk <1994constant@gmail.com>
2022-03-19 00:47:40 +0530

    feat: Add v0 soulbound NFT Merkle drop

---
+++ contracts/MerkleSoulbound.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title Merkle Soulbound NFT Drop
+/// @author QEDK
+/// @notice This contract takes an existing merkle root and allows them to claim from the tranche of NFTs.
+/// @custom:experimental This is an experimental contract.
+contract MerkleSoulboundNFTDrop is ERC721URIStorage, Ownable {
+    uint256 trancheId;
+    uint256 tokenId;
+    mapping(uint256 => bytes32) trancheMerkles;
+    mapping(uint256 => string) trancheUris;
+    mapping(uint256 => mapping(address => bool)) trancheClaims;
+
+    constructor() ERC721("MerkleSoulboundNFT", "MSN") {
+
+    }
+
+    function addTranche(bytes32 _merkleRoot, string calldata _uri) external onlyOwner {
+        trancheUris[trancheId] = _uri;
+        trancheMerkles[trancheId] = _merkleRoot;
+        trancheId++;
+    }
+
+    function modifyTranche(bytes32 _newMerkleRoot, string calldata _newUri) external onlyOwner {
+        trancheUris[trancheId] = _newUri;
+        trancheMerkles[trancheId] = _newMerkleRoot;
+    }
+
+    function deleteTranche(uint256 _id) external onlyOwner {
+        delete trancheMerkles[_id];
+        delete trancheUris[_id];
+    }
+
+    function expireTranche(uint256 _id) external onlyOwner {
+        delete trancheMerkles[_id];
+    }
+
+    function claimFromTranche(uint256 _id, bytes32[] calldata merkleProof) external {
+        require(trancheClaims[_id][msg.sender] == false, "ALREADY_CLAIMED");
+        trancheClaims[_id][msg.sender] = true;
+        require(MerkleProof.verify(merkleProof, trancheMerkles[_id], keccak256(abi.encodePacked(msg.sender))), "INVALID_MERKLE_PROOF");
+        _mint(msg.sender, tokenId); // _mint instead of _safeMint so contract wallets are able to receive
+        _setTokenURI(tokenId, trancheUris[_id]);
+        tokenId++;
+    }
+
+    function _transfer(
+        address /* from */,
+        address /* to */,
+        uint256 /* tokenId */
+    ) internal pure override {
+        revert("SOULBOUND_NFT_TRANSFER_DISALLOWED");
+    }
+}
